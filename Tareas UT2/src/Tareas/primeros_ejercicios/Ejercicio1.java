8ac310c3073c60df62c31ca1f69e3c411c50eae1
Marta <Marta@Equipo-Marta>
2013-10-17 17:29:46 +0200

    cambios en el 1

--- Tareas UT2/src/Tareas/primeros_ejercicios/Ejercicio1.java
+++ Tareas UT2/src/Tareas/primeros_ejercicios/Ejercicio1.java
@@ -7,6 +7,7 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//cambio
 System.out.print("Hola, ¿Como te llamas?");
 		
 		Scanner teclado=new Scanner(System.in);
